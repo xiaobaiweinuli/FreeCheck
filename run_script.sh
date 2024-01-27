29d50bf7ee0d1ac443d26c936cb22bc1957f56bf
xiaobaiweinuli <94781176+xiaobaiweinuli@users.noreply.github.com>
2024-01-27 20:47:24 +0800

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 设置变量，您可以根据需要更改这些值
-USERID_1="$1"
-TOKEN_1="$2"
-USERID_2="$3"
-TOKEN_2="$4"
-PUSHPLUS_TOKEN="$5"
+USERID_1="$USERID_1"
+TOKEN_1="$TOKEN_1"
+USERID_2="$USERID_2"
+TOKEN_2="$TOKEN_2"
+PUSHPLUS_TOKEN="$PUSHPLUS_TOKEN"
 NOTIFICATIONS=()
 USER_COUNT=0  # Counter to keep track of the number of users processed
 TOTAL_USERS=2  # Set the total number of users
@@ -14,10 +14,7 @@ TOTAL_USERS=2  # Set the total number of users
 function pushplus_notification() {
     local title="$1"
     local content="$2"
-    curl -X POST "http://www.pushplus.plus/send" \
-        -H "Content-Type: application/json" \
-        -d "{\"token\":\"$PUSHPLUS_TOKEN\",\"title\":\"$title\",\"content\":\"$content\"}" \
-        -s
+    curl -s -X POST "http://www.pushplus.plus/send" -d "token=$PUSHPLUS_TOKEN&title=$title&content=$content&template=markdown"
 }
 
 # 随机等待函数
@@ -34,8 +31,6 @@ function execute_user_operations() {
     local token="$2"
     local user_note="$3" # Add a parameter for user note
     
-    echo "********运行账号$user_note ($userid)********"
-    
     # 获取当前时间戳
     local timestamp=$(date +%s)
     echo "选择qd_info的时间戳：$timestamp"
@@ -48,41 +43,26 @@ function execute_user_operations() {
         -d "$timestamp" \
         -s) 
     
-    echo "qd_info的JSON 响应内容：$response"
-    
     local record_id=$(echo "$response" | jq -r '.data.record_id')
-    echo "提取 record_id 的值：$record_id"
     
     local sortIndex=$(echo "$response" | jq -r '.data.list[] | select(.state == 0) | .sortIndex')
     
-    echo "提取 sortIndex 的值：$sortIndex"
-    
     local first_sortIndex=$(echo "$sortIndex" | head -n 1)
     
-    echo "选择第一个sortIndex：$first_sortIndex"
-    
     local updated_timestamp=$(date +%s)
-    echo "set_qd的时间戳：$updated_timestamp"
     
     local json_payload="{\"qd_id\":$record_id,\"sort_index\":$first_sortIndex,\"Timestamp\":$updated_timestamp}"
-    echo "组成JSON_PAYLOAD：$json_payload"
     
-    # 发送 POST 请求
     local api_response=$(curl -X POST 'https://m.freecheck.cn/api/user/set_qd' \
         -H 'User-Agent: Mozilla/5.0 (Linux; Android 13; 23013RK75C Build/TKQ1.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160043 MMWEBSDK/20230805 MMWEBID/2038 MicroMessenger/8.0.42.2424(0x28002A43) WeChat/arm64 Weixin GPVersion/1 NetType/WIFI Language/zh_CN ABI/arm64' \
         -H 'Content-Type: application/json' \
         -H "userid: $USERID_1" \
         -H "token: $TOKEN_1" \
         -d "$json_payload" \
-        -s) # 使用 -s 参数静默模式输出
-
-    # 打印当前账号的 API 响应
-    echo "账号$user_note ($userid) 的 API 响应：$api_response"
+        -s) 
     
     # 将通知添加到数组
     NOTIFICATIONS+=("$user_note ($userid)，$api_response")
-    
-    echo "********结束账号$user_note ($userid) 操作********"
     
     # Increment the user counter
     ((USER_COUNT++))
